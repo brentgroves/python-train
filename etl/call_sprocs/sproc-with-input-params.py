0f3841c45a8713df6372a3394488608b212be96e
Brent Groves <brent.groves@gmail.com>
2022-08-02 16:10:07 -0400

    Added source code

--- etl/call_sprocs/sproc-with-input-params.py
+++ etl/call_sprocs/sproc-with-input-params.py
@@ -7,16 +7,19 @@
 # https://docs.python-zeep.org/en/master/
 import pyodbc 
 from datetime import datetime
-import sys 
+import sys
 import mysql.connector
 from mysql.connector import Error
 
+from sqlalchemy import true 
 # https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-3-proof-of-concept-connecting-to-sql-using-pyodbc?view=sql-server-ver16
 # https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/programming-guidelines?view=sql-server-ver16
 # remember to source oaodbc64.sh to set env variables.
 # https://github.com/mkleehammer/pyodbc/wiki/Calling-Stored-Procedures
 # https://thepythonguru.com/fetching-records-using-fetchone-and-fetchmany/
 # https://code.google.com/archive/p/pyodbc/wikis/Cursor.wiki
+# https://code.google.com/archive/p/pyodbc/wikis/Connection.wiki
+
 def print_to_stdout(*a):
     # Here a is the array holding the objects
     # passed as the argument of the function
@@ -31,22 +34,22 @@ def print_to_stderr(*a):
 try:
     ret = 0
     # pcn = (sys.argv[1])
-    # username = (sys.argv[2])
-    # password = (sys.argv[3])
-    # username2 = (sys.argv[4])
-    # password2 = (sys.argv[5])
-    # username3 = (sys.argv[6])
-    # password3 = (sys.argv[7])
+    # username2 = (sys.argv[2])
+    # password2 = (sys.argv[3])
+    # username3 = (sys.argv[4])
+    # password3 = (sys.argv[5])
 
-    # pcn = '300758'
     pcn = '123681'
-    username = 'mg.odbcalbion'
-    password = 'Mob3xalbion'
     username2 = 'mgadmin'
     password2 = 'WeDontSharePasswords1!'
     username3 = 'root'
     password3 = 'password'   
-    # print(f"params={params}")
+
+    # params = (sys.argv[1])
+    # username = (sys.argv[2])
+    # password = (sys.argv[3])
+    # username2 = (sys.argv[4])
+    # password2 = (sys.argv[5])
     # print(f"params={params}")
     # print(f"params={params},username={username},password={password},username2={username2},password2={password2}")
     # sys.exit(0)
@@ -58,98 +61,53 @@ try:
     print_to_stdout(f"Current Time: {current_time=}")
 
     conn3 = mysql.connector.connect(user=username3, password=password3,
-                            host='10.1.0.116',
-                            port='31008',
-                            database='Plex')
+                        host='10.1.0.116',
+                        port='31008',
+                        database='Plex')
 
     cursor3 = conn3.cursor()
     period_start = 0
     period_end = 0
-    open_period = 0
     no_update = 9
     # The parameters are needed in the call but the output params are not changed but are in result_args.
-    result_args =cursor3.callproc('accounting_balance_get_period_range', [pcn,period_start,period_end,open_period,no_update])
+    result_args =cursor3.callproc('accounting_balance_get_period_range', [pcn,period_start,period_end,no_update])
     # result_args =cursor3.callproc('accounting_balance_get_period_range', [123681,period_start2,period_end2])
     #  https://www.mysqltutorial.org/calling-mysql-stored-procedures-python/
     period_start = result_args[1] #param 2
     period_end = result_args[2] #param 3
-    # open_period = result_args[3] #param 4
-    no_update = result_args[4] #param 5
+    no_update = result_args[3] #param 4
     # print(f"PLSQL: period_start={result_args[1]} period_end={result_args[2]}")
 
     if no_update != 1:
-
-        # https://docs.microsoft.com/en-us/sql/connect/python/pyodbc/step-1-configure-development-environment-for-pyodbc-python-development?view=sql-server-ver15
-        # password = 'wrong' 
-        conn = pyodbc.connect('DSN=Plex;UID='+username+';PWD='+ password)
-        # https://stackoverflow.com/questions/11451101/retrieving-data-from-sql-using-pyodbc
-        cursor = conn.cursor()
-    
-        # accounting_balance_append_period_range_dw_import
-        rowcount=cursor.execute("{call sproc300758_11728751_2000117 (?,?,?)}", pcn,period_start,period_end).rowcount
-        rows = cursor.fetchall()
-        print_to_stdout(f"call sproc300758_11728751_2000117 - rowcount={rowcount}")
-        print_to_stdout(f"call sproc300758_11728751_2000117 - messages={cursor.messages}")
-
-        cursor.close()
-        fetch_time = datetime.now()
-        tdelta = fetch_time - start_time 
-        print_to_stdout(f"fetch_time={tdelta}") 
-
-        conn2 = pyodbc.connect('DSN=dw;UID='+username2+';PWD='+ password2 + ';DATABASE=mgdw')
+        # https://www.pythonfixing.com/2022/02/fixed-how-to-set-db-connection-timeout.html
+        conn2 = pyodbc.connect('DSN=dw;UID='+username2+';PWD='+ password2 + ';DATABASE=mgdw',timeout=30)
+        # conn2.timeout = 10
+        # conn2.autocommit = True
         cursor2 = conn2.cursor()
+        # https://code.google.com/archive/p/pyodbc/wikis/GettingStarted.wiki
+        rowcount=cursor2.execute("{call Plex.account_period_balance_recreate_period_range (?)}",pcn).rowcount
 
-        tsql = """\
-        EXEC Plex.accounting_balance_delete_period_range @pcn = ?;
-        """
-        cursor2.execute(tsql, (pcn)).rowcount
-
-        # cursor2 = conn2.cursor()
-        # # https://code.google.com/archive/p/pyodbc/wikis/GettingStarted.wiki
-        # rowcount=cursor2.execute("{call Plex.accounting_balance_delete_period_range}").rowcount
-        # rowcount=cursor2.execute("{call Scratch.accounting_balance_delete_period_range}").rowcount
         # https://github.com/mkleehammer/pyodbc/wiki/Cursor
         # The return value is always the cursor itself:
-        print_to_stdout(f"call Plex.accounting_balance_delete_period_range - rowcount={rowcount}")
-        print_to_stdout(f"call Plex.accounting_balance_delete_period_range - messages={cursor2.messages}")
-
+        print_to_stdout(f"call Plex.account_period_balance_recreate_period_range - rowcount={rowcount}")
+        print_to_stdout(f"call Plex.account_period_balance_recreate_period_range - messages={cursor2.messages}")
         cursor2.commit()
-
+        cursor2.close()
         # https://github.com/mkleehammer/pyodbc/wiki/Cursor
         # https://github.com/mkleehammer/pyodbc/wiki/Features-beyond-the-DB-API#fast_executemany
         # https://towardsdatascience.com/how-i-made-inserts-into-sql-server-100x-faster-with-pyodbc-5a0b5afdba5
 
-        im2 = '''INSERT INTO Plex.accounting_balance (pcn, account_key, account_no, period, debit, credit, balance)
-                VALUES(?, ?, ?, ?, ?, ?, ?);'''
-        # im2 = '''INSERT INTO Scratch.accounting_balance (pcn, account_key, account_no, period, debit, credit, balance)
-        #         VALUES(?, ?, ?, ?, ?, ?, ?);'''
+        # https://towardsdatascience.com/how-i-made-inserts-into-sql-server-100x-faster-with-pyodbc-5a0b5afdba5
 
 
-        cursor2.fast_executemany = True
-        cursor2.executemany(im2,rows)
-        cursor2.commit()
-        cursor2.close()
-
-        insertObject = []
-        # columnNames = [column[0] for column in cursor.description]
-        for record in rows:
-            insertObject.append(tuple(record))
-
-        # cursor2.callproc('get_laptop', [1, ])
-        cursor3.callproc('accounting_balance_delete_period_range', [pcn])
+        cursor3.callproc('account_period_balance_recreate_period_range', [pcn])
         # rowcount=cursor2.execute(txt.format(dellist = params)).rowcount
-        print_to_stdout(f"call Plex.accounting_balance_delete_period_range - rowcount={cursor3.rowcount}")
+        print_to_stdout(f"call Plex.account_period_balance_recreate_period_range() - rowcount={cursor3.rowcount}")
         # print_to_stdout(f"{txt} - messages={cursor2.messages}")
-        conn3.commit()
-
-        im2 = '''INSERT INTO Plex.accounting_balance (pcn, account_key, account_no, period, debit, credit, balance)
-                VALUES(%s,%s,%s,%s,%s,%s,%s);'''
-        cursor3.executemany(im2,insertObject)
-        # cursor2.executemany(im2,records_to_insert)
         conn3.commit()
         cursor3.close()
 
-    # https://towardsdatascience.com/how-i-made-inserts-into-sql-server-100x-faster-with-pyodbc-5a0b5afdba5
+
 except pyodbc.Error as ex:
     ret = 1
     error_msg = ex.args[1]
@@ -158,7 +116,7 @@ except pyodbc.Error as ex:
 
 except Error as e:
     ret = 1
-    print("MySQL error: ", e)
+    print("Error while connecting to MySQL", e)
 
 except BaseException as error:
     ret = 1
@@ -168,8 +126,7 @@ finally:
     end_time = datetime.now()
     tdelta = end_time - start_time 
     print_to_stdout(f"total time: {tdelta}") 
-    if 'conn' in globals():
-        conn.close()
+    # print_to_stdout(f"before the commit")
     if 'conn2' in globals():
         conn2.close()
     if 'conn3' in globals():
---
+++ how_to/to_str.py
@@ -0,0 +1,12 @@
+# An integer version of year
+integer_year = 2019
+
+# A string version of year
+string_year = str(2019)
+
+# Concatinates the string with the string version of year
+print("This is " + string_year + ".")
+
+
+# Returns none if no argument is provided
+print(str())
